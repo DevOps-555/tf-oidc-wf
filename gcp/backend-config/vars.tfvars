e8fd8bde47514888d1b3e522ff5ec209ce6aacdd
Vivek <vivek@gmail.com>
2023-01-05 01:12:03 +0530

    auth 14.0

--- gcp/backend-config/vars.tfvars
+++ gcp/backend-config/vars.tfvars
@@ -1,4 +1,4 @@
-gcp_project     = "nth-glass-338610"
+gcp_project = "nth-glass-338610"
 
 gh_repo = "vivekreddy0808/tf-oidc-wf"
 gh_branch = "refs/heads/main"
--- gcp/tf-sa/provider.tf
+++ gcp/tf-sa/provider.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.45.0"
+      version = "4.47.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "4.45.0"
+      version = "4.47.0"
     } 
   }
 }
